b083c812f91ed2d174398d967f228b50366d3c5b
hrh94 <1131194920@qq.com>
2017-11-23 17:51:47 +0800

    webpack-your-bags "我们现在将把它做活起来"前

---
+++ webpack-your-bags-v4/src/index.js
@@ -0,0 +1,27 @@
+// import $ from 'jquery';
+// $('body').html('Hello');
+
+// import Button from './Components/Button';
+// const button = new Button('google.com');
+// button.render('a');
+
+import $ from 'jquery';
+import './styles.scss';
+
+if (document.querySelectorAll('a').length) {
+    require.ensure([], () => {
+        const Button = require('./Components/Button').default;
+        const button = new Button('google.com');
+
+        button.render('a');
+    });
+}
+
+// If we have a title, render the Header component on it
+if (document.querySelectorAll('h1').length) {
+    require.ensure([], () => {
+        const Header = require('./Components/Header').default;
+
+        new Header().render('h1');
+    });
+}
